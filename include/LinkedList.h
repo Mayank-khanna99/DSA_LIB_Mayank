d7bea9a0ee2c1cb689d4249ac4bf29e3de2c76cb
Mayank Khanna <mayank@cqst.in>
2026-07-02 11:16:11 +0530

    Implemented push_back and push_front functionality

---
+++ include/LinkedList.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include<stdexcept>
+#include<iostream>
+#include"Allocator.h"
+
+template<typename T>
+class LinkedList{
+    private:
+        class Node{
+            public:
+                T data;
+                Node* next;
+                Node(const T& value){
+                    data=value; 
+                    next=nullptr;
+                }
+        };
+        MyAllocator<Node> allocator;
+        Node* head;
+        Node* tail;
+        int length;
+    public:
+        LinkedList(){
+            head=nullptr;
+            tail=nullptr;
+            length=0;
+        }
+        void push_front(const T& value){
+            Node* newnode = allocator.Allocate();
+            allocator.Construct(newnode,value);
+            if(head==nullptr){
+                head=newnode;
+                tail=newnode;
+                length++;
+            }else{
+                newnode->next=head;
+                head=newnode;
+                length++;
+            }
+        }
+
+        void push_back(const T& value){
+            Node* newnode = allocator.Allocate();
+            allocator.Construct(newnode,value);
+            if(head==nullptr && tail==nullptr){
+                head=newnode;
+                tail=newnode;
+            }
+            else if(head==tail){
+                tail->next=newnode;
+                tail=newnode;
+            }else{
+                tail->next=newnode;
+                tail=newnode;
+            }
+            length++;
+        }
+
+        void print(){
+            Node* temp=head;
+            while(temp!=nullptr){
+                std::cout<<temp->data<<"->";
+                temp=temp->next;
+            }
+        }
+
+};
