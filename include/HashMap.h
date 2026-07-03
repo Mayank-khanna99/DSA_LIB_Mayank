2aa93012a4b1758646c8c9c3ce5fb058d5cdd31a
Mayank Khanna <mayank@cqst.in>
2026-07-03 12:04:23 +0530

    Implemented Insert and Rehash methods in HashMap

--- include/HashMap.h
+++ include/HashMap.h
@@ -39,6 +39,54 @@ class HashMap{
             }
             return nullptr;
         }
+        const Entry* findEntry(const K& key)const{
+            size_t index=bucketIndex(key);
+            for(auto it = buckets[index].begin(); it != buckets[index].end(); ++it){
+                if((*it).key == key)
+                {
+                    return &(*it);
+                }
+            }
+            return nullptr;
+        }
+
+        void rehash(){
+            // Save old data
+            LinkedList<Entry>* oldBuckets = buckets;
+            size_t oldCapacity = currentCapacity;
+
+            // Double capacity
+            currentCapacity *= 2;
+
+            // Allocate new buckets
+            buckets = allocator.Allocate(currentCapacity);
+
+            for(size_t i = 0; i < currentCapacity; i++)
+            {
+                allocator.Construct(&buckets[i]);
+            }
+
+            // Reset size
+            currentSize = 0;
+
+            // Move every entry to new buckets
+            for(size_t i = 0; i < oldCapacity; i++)
+            {
+                for(auto it = oldBuckets[i].begin(); it != oldBuckets[i].end(); ++it)
+                {
+                    insert((*it).key, (*it).value);
+                }
+            }
+
+            // Destroy old buckets
+            for(size_t i = 0; i < oldCapacity; i++)
+            {
+                allocator.Destroy(&oldBuckets[i]);
+            }
+
+            allocator.Deallocate(oldBuckets);
+        }
+        
         public:
             HashMap(){
                 currentCapacity=8;
@@ -46,6 +94,28 @@ class HashMap{
                 buckets = allocator.Allocate(currentCapacity);
                 for (size_t i = 0; i < currentCapacity; i++){
                     allocator.Construct(&buckets[i]);
+                }
+            }
+            void insert(const K& key, const V& value){
+                Entry* existing = findEntry(key);
+
+                if(existing != nullptr)
+                {
+                    existing->value = value;
+                    return;
+                }
+
+                size_t index = bucketIndex(key);
+
+                Entry entry(key, value);
+
+                buckets[index].push_back(entry);
+
+                currentSize++;
+
+                if(loadFactor() > MAX_LOAD_FACTOR)
+                {
+                    rehash();
                 }
             }
             ~HashMap(){
